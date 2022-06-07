01e8feb3c76fdd87825b43981c1956296c68280b
Jan Unar <johnunar@gmail.com>
2022-06-07 19:11:12 +0200

    Updated scraper code, optimised the prime finder

--- 1_elementary.py
+++ 1_elementary.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 
@@ -28,6 +29,7 @@ def hello_six_names(name1, name2, name3, name4, name5=None, name6=None):
 def hello_names(*names):
     """
     Prints 'Hello, {names}'
+
     :param names:
     :return:
     """
@@ -38,6 +40,7 @@ def hello_names(*names):
 def shopping_list(**kwargs):
     """
     Prints a shopping list
+
     :param kwargs:
     :return:
     """
@@ -50,7 +53,12 @@ def shopping_list(**kwargs):
     print("Total:", item_count, "kg")
 
 
-def sum_numbers(num):
+def sum_numbers(num: int) -> int:
+    """
+    Sums all numbers below given number (incl.)
+
+    :param num: Number to process the sum for
+    """
     result = 0
     for number in range(1, num + 1):
         result += number
@@ -58,31 +66,53 @@ def sum_numbers(num):
     return result
 
 
-def sum_numbers_py(num):
+def sum_numbers_py(num: int) -> int:
+    """Same as sum_numbers() but using a Python built-in function"""
     return sum(range(1, num + 1))
 
 
-def get_numbers(num, type):
+def get_numbers_by_parity(num: int, parity: str) -> list:
+    """
+    Generated a list of odd or even numbers below given number
+
+    :param num: All numbers will be below this number
+    :param parity: "odd" or "even"
+    :return: List with odd or even numbers below given number
+    """
     result = []
     for number in range(1, num + 1):
-        if type == "odd" and number % 2 != 0:
+        if parity == "odd" and number % 2 != 0:
             result.append(number)
-        if type == "even" and number % 2 == 0:
+        if parity == "even" and number % 2 == 0:
             result.append(number)
 
     return result
 
 
-def is_prime(num):
+def is_prime(num: int) -> bool:
+    """
+    Checks whether the given integer is prime
+
+    :param num: Integer to check
+    :return: True if prime, False if not
+    """
+    # IDEA: Check the 'sieve of Eratosthenes' for further optimisation
     result = True
-    for number in range(2, num):
+    for number in range(2, math.floor(math.sqrt(num))):
         if num % number == 0:
             result = False
+            break
 
     return result
 
 
-def get_primes(num):
+def get_primes(num: int) -> list:
+    """
+    Generates prime numbers below given number
+
+    :param num: All numbers will be below this number
+    :return: List with prime numbers below given number
+    """
     result = []
     for number in range(1, num + 1):
         if is_prime(number):
@@ -94,17 +124,22 @@ def get_primes(num):
 if __name__ == "__main__":
     hello()
     print("----------------")
+
     hello_name("Alice")
     print("----------------")
+
     hello_six_names("Alice", "Bob", "Pepa", "Kaja", name6="Pavel", name5="Petr")
     print("----------------")
+
     hello_names("Franta", "Petr", "Max")
     print("----------------")
+
     shopping_list(bananas=5, strawberries=15, apples=66, yogurt=2)
     print("----------------")
+
     print(sum_numbers(10))
     start = time.time()
-    print(sum_numbers(100000000))
+    print(sum_numbers(10000))
     end = time.time()
     print("For loop: ", end - start)
     start = time.time()
@@ -112,9 +147,18 @@ if __name__ == "__main__":
     end = time.time()
     print("Sum Function: ", end - start)
     print("----------------")
-    print(get_numbers(100, "even"))
-    print(get_numbers(100, "odd"))
-    print(sum(get_numbers(100, "even")))
-    print(sum(get_numbers(100, "odd")))
+
+    print(get_numbers_by_parity(100, "even"))
+    print(get_numbers_by_parity(100, "odd"))
+    print(sum(get_numbers_by_parity(100, "even")))
+    print(sum(get_numbers_by_parity(100, "odd")))
     print("----------------")
-    print(get_primes(50000))
+
+    # With no optimisations (no break, no sqrt()): ~ 20 000 under 10s
+    # With break upon finding a divisor: ~ 60 000 under 10s
+    # With break and a limitation of checked numbers: ~ 2 500 000 under 10s
+    start = time.time()
+    get_primes(2500000)
+    end = time.time()
+    print("2500000: ", end - start)
+    print("----------------")
--- 3_scraping.py
+++ 3_scraping.py
@@ -3,22 +3,28 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 
-URL = "http://quotes.toscrape.com/"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+def print_all_quotes():
+    """Prints all quotes that can be found at http://quotes.toscrape.com/"""
+    page_number = 1
+    while True:
+        URL = f"http://quotes.toscrape.com/page/{page_number}/"
+        page = requests.get(URL)
 
-results = soup.find_all("div", {"class": "quote"})
+        soup = BeautifulSoup(page.content, "html.parser")
 
-# print(
-#     results[0].find("span").text,
-#     results[0].find_all("span")[1].find("small").text,
-#     URL + results[0].find_all("span")[1].find("a")["href"],
-# )
+        results = soup.find_all("div", {"class": "quote"})
+        for result in results:
+            print(
+                result.find("span").text,
+                result.find_all("span")[1].find("small").text,
+                urljoin(URL, result.find_all("span")[1].find("a")["href"]),
+            )
+        if page_number == 10:
+            break
 
-for result in results:
-    print(
-        result.find("span").text,
-        result.find_all("span")[1].find("small").text,
-        urljoin(URL, result.find_all("span")[1].find("a")["href"]),
-    )
+        page_number += 1
+
+
+if __name__ == '__main__':
+    print_all_quotes()
